2f45a0c9ce4eb2d47195bdd8e59e7169e2d70140
Roger Grajales <roger.grajalesg@gmail.com>
2023-02-22 22:41:58 -0500

    Create 01mensaje_en_pantalla.py
    01mensaje_en_pantalla

---
+++ 01mensaje_en_pantalla.py
@@ -0,0 +1,9 @@
+print("\nLa felicidad se puede encontrar hasta en los más oscuros\nmomentos, si somos capaces de usar bien la luz.\n— Albus Dumbledore\n")
+
+print('''\nLa felicidad se puede encontrar hasta en los más oscuros
+momentos, si somos capaces de usar bien la luz.
+— Albus Dumbledore\n''')
+
+print("""\nLa felicidad se puede encontrar hasta en los más oscuros
+momentos, si somos capaces de usar bien la luz.
+— Albus Dumbledore\n""")
